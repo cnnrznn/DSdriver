4279975ada44345a311c96ca03f3821b87106040
Connor Zanin <cnnrznn@gmail.com>
2019-10-22 09:39:31 -0400

    gofmt code

--- dsdriver.go
+++ dsdriver.go
@@ -1,60 +1,59 @@
 package dsdriver
 
 import (
-    "math/rand"
-    "time"
+	"math/rand"
+	"time"
 )
 
 type Dester interface {
-    Dest() int          // a function that reports the destination of this thing
+	Dest() int // a function that reports the destination of this thing
 }
 
-type Hub func(sendChans []chan Dester, recvChan chan Dester) ()
+type Hub func(sendChans []chan Dester, recvChan chan Dester)
 
 func BenignHub(sendChans []chan Dester, recvChan chan Dester) {
-    for {
-        select {
-        case d := <-recvChan:
-            sendChans[d.Dest()] <- d
-        }
-    }
+	for {
+		select {
+		case d := <-recvChan:
+			sendChans[d.Dest()] <- d
+		}
+	}
 }
 
 func ReorderHub(sendChans []chan Dester, recvChan chan Dester) {
-    buffer := make([]Dester, 0)
-    startTime := time.Now()
-    timeDiff := 100 * time.Millisecond
+	buffer := make([]Dester, 0)
+	startTime := time.Now()
+	timeDiff := 100 * time.Millisecond
 
-    for {
-        select {
-        case d := <-recvChan:
-            buffer = append(buffer, d)
-        default:
-            if time.Now().Sub(startTime) > timeDiff {
-                startTime = time.Now()
-                rand.Shuffle(len(buffer), func(i, j int) {
-                        buffer[i], buffer[j] = buffer[j], buffer[i]
-                })
-                for len(buffer) > 0 {
-                    d := buffer[0]
-                    buffer = buffer[1:]
-                    sendChans[d.Dest()] <- d
-                }
-            }
-        }
-    }
+	for {
+		select {
+		case d := <-recvChan:
+			buffer = append(buffer, d)
+		default:
+			if time.Now().Sub(startTime) > timeDiff {
+				startTime = time.Now()
+				rand.Shuffle(len(buffer), func(i, j int) {
+					buffer[i], buffer[j] = buffer[j], buffer[i]
+				})
+				for len(buffer) > 0 {
+					d := buffer[0]
+					buffer = buffer[1:]
+					sendChans[d.Dest()] <- d
+				}
+			}
+		}
+	}
 }
 
 func Local(n int, fn Hub) (frChan chan Dester,
-                           toChans []chan Dester) {
-    frChan = make(chan Dester, 1024)
+	toChans []chan Dester) {
+	frChan = make(chan Dester, 1024)
 
-    for i := 0; i < n; i++ {
-        toChans = append(toChans, make(chan Dester, 1024))
-    }
+	for i := 0; i < n; i++ {
+		toChans = append(toChans, make(chan Dester, 1024))
+	}
 
-    go fn(toChans, frChan)
+	go fn(toChans, frChan)
 
-    return
+	return
 }
-
